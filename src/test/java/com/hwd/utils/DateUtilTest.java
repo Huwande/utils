f0ff24d332e3332d311e095928197cbc16cc97b7
hwd <1105185293@qq.com>
2019-09-09 14:37:26 +0800

    工具类

---
+++ src/test/java/com/hwd/utils/DateUtilTest.java
@@ -0,0 +1,61 @@
+package com.hwd.utils;
+
+import static org.junit.Assert.fail;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import org.junit.Test;
+
+public class DateUtilTest {
+
+	@Test
+	public void testRandomDate() {
+		Calendar c = Calendar.getInstance();
+		c.set(2010, 0, 1);
+		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+	
+		for (int i = 0; i <10; i++) {
+		
+			Date date = DateUtil.randomDate(c.getTime(), new Date());
+			String string = df.format(date);
+			System.out.println(string);
+			
+		}
+	}
+
+	@Test
+	public void testGetDateByInitMonth() {
+		Calendar c = Calendar.getInstance();
+		c.set(2000, 9, 10);
+		Date date = DateUtil.getDateByInitMonth(c.getTime());
+		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		String string = df.format(date);
+		
+		System.out.println(string);
+	}
+
+	@Test
+	public void testGetDateByFullMonth() {
+		Calendar c = Calendar.getInstance();
+		c.set(2008, 1, 22);
+		
+		Date date = DateUtil.getDateByFullMonth(c.getTime());
+		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		String string = df.format(date);
+		
+		System.out.println(string);
+	}
+	@Test
+	public void testSql() {
+		String sql = "select * from t_order where create_time>='{1}' and create_time<='{2}' ";
+		Date date = DateUtil.getDateByInitMonth(new Date());
+		Date date2 = DateUtil.getDateByFullMonth(new Date());
+		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		String string = sql.replace("{1}",df.format(date)).replace("{2}", df.format(date2));
+		System.out.println(string);
+		
+	}
+
+}
---
+++ src/test/java/com/hwd/utils/StringUtilTest.java
@@ -0,0 +1,63 @@
+package com.hwd.utils;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class StringUtilTest {
+
+	@Test
+	public void testToHtml() {
+		String a="哇哦打卡     ";
+		String html = StringUtil.toHtml(a);
+		System.out.println(html);
+	}
+
+	@Test
+	public void testIsPhoneNumber() {
+		String b="17600023513";
+		boolean c = StringUtil.isPhoneNumber(b);
+		System.out.println(c);
+	}
+
+	@Test
+	public void testIsEmail() {
+		String cString="1105185293@qq.com";
+		boolean email = StringUtil.isEmail(cString);
+		System.out.println(email);
+	}
+
+	@Test
+	public void testHasLength() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testHasText() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRandomSimpleChinese() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRandomChineseString() {
+		 for(int i=0;i<10;i++) {
+			 String string = StringUtil.randomChineseString();
+			 System.out.print(string+" ");
+		 }
+	}
+
+	@Test
+	public void testRandomChineseStringInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGenerateChineseName() {
+		fail("Not yet implemented");
+	}
+
+}
